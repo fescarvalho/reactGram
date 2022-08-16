980cc95426980ad9ab0f1acd25ca8be8685dab0c
Fernando Carvalho <fernando_scarvalho@hotmail.com>
2022-08-15 21:44:05 -0300

    creation route of login, register, creation middlewares autentications, and validations

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -12,9 +12,67 @@ const generateToken = (id) => {
 
 //Register user and sign in
 const register = async (req, res) => {
-  res.send("Registro");
+  const { name, email, password } = req.body;
+
+  //check if user exists
+  const user = await User.findOne({ email });
+  if (user) {
+    res.status(422).json({ errors: ["Por favor, utilize outro email"] });
+    return;
+  }
+
+  //Generate password hash
+  const salt = await bcrypt.genSalt();
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  //Create User
+  const newUser = await User.create({
+    name,
+    email,
+    password: passwordHash,
+  });
+
+  //If user successfully created, return the token
+  if (!newUser) {
+    res
+      .status(422)
+      .json({ errors: ["Houve um erro, por favor, tente mais tarde."] });
+    return;
+  }
+
+  res.status(201).json({
+    _id: newUser._id,
+    token: generateToken(newUser._id),
+  });
+};
+
+//Sing In
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  //Check for user exist
+  if (!user) {
+    res.status(404).json({ errors: ["Usuario não encontrado."] });
+    return;
+  }
+
+  //Check password matches
+  if (!(await bcrypt.compare(password, user.password))) {
+    res.status(422).json({ errors: ["Senha Invalida."] });
+    return;
+  }
+
+  //Return this with token
+  res.status(201).json({
+    _id: user._id,
+    profileImage: user.profileImage,
+    token: generateToken(user._id),
+  });
 };
 
 module.exports = {
   register,
+  login,
 };
