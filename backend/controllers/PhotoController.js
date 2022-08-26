4e87380f0f354b85e22f4d62cf052224c51928f9
fescarvalho <fernando_scarvalho@hotmail.com>
2022-08-26 20:58:33 -0300

    create a function of att title of photo

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -42,8 +42,8 @@ const deletePhoto = async (req, res) => {
     }
 
     //Check photo belongs to user
-    if (!photo.userId.equals(reqUser.id)) {
-      res.status(422).json({ erros: ['Ocorreu um erro, por favor tente novamente mais tarde'] });
+    if (!photo.userId.equals(reqUser._id)) {
+      res.status(422).json({ errors: ['Ocorreu um erro, por favor tente novamente mais tarde'] });
     }
 
     await Photo.findByIdAndDelete(photo._id);
@@ -87,10 +87,39 @@ const getPhotoById = async (req, res) => {
   res.status(200).json(photo);
 };
 
+//Upadte a photo
+const updatePhoto = async (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const reqUser = req.user;
+  const photo = await Photo.findById(id);
+
+  //Check if photo exists
+  if (!photo) {
+    res.status(404).json({ errors: ['Foto não encontrada.'] });
+    return;
+  }
+
+  //Check photo belongs to user
+  if (!photo.userId.equals(reqUser._id)) {
+    res.status(422).json({ errors: ['Ocorreu um erro, por favor tente novamente mais tarde'] });
+    return;
+  }
+
+  if (title) {
+    photo.title = title;
+  }
+
+  await photo.save();
+  res.status(200).json({ photo, message: 'Foto atualizada com sucesso!' });
+};
+
 module.exports = {
   insertPhoto,
   deletePhoto,
   getAllPhotos,
   getUserPhotos,
   getPhotoById,
+  updatePhoto,
 };
--- backend/middlewares/photoValidation.js
+++ backend/middlewares/photoValidation.js
@@ -19,4 +19,15 @@ const photoInsertValidation = () => {
   ];
 };
 
-module.exports = { photoInsertValidation };
+const photoUpdateValidation = () => {
+  return [
+    body('title')
+      .optional()
+      .isString()
+      .withMessage('O titulo é obrigatório.')
+      .isLength({ min: 3 })
+      .withMessage('O titulo precisa ter no minimo 3 caracteres.'),
+  ];
+};
+
+module.exports = { photoInsertValidation, photoUpdateValidation };
