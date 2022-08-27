9687ebd95e4c4e67d63fa72e3ea2ebdac31ad685
fescarvalho <fernando_scarvalho@hotmail.com>
2022-08-26 21:23:31 -0300

    create a function of liked a photo

--- backend/routes/PhotoRouters.js
+++ backend/routes/PhotoRouters.js
@@ -9,6 +9,7 @@ const {
   getUserPhotos,
   getPhotoById,
   updatePhoto,
+  likePhoto,
 } = require('../controllers/PhotoController');
 
 //Middlewaresre
@@ -32,4 +33,5 @@ router.get('/', authGuard, getAllPhotos);
 router.get('/user/:id', authGuard, getUserPhotos);
 router.get('/:id', authGuard, getPhotoById);
 router.put('/:id', authGuard, photoUpdateValidation(), validate, updatePhoto);
+router.put('/like/:id', authGuard, likePhoto);
 module.exports = router;
