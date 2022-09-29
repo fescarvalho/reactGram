0ce84431d6274f1de3bac3a634e35020481fd838
fescarvalho <fernando_scarvalho@hotmail.com>
2022-09-28 21:48:39 -0300

    config db modifyed

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 //connection
 const conn = async () => {
   try {
-    const dbConn = await mongoose.connect(process.env.DB_CONNECT);
+    const dbConn = await mongoose.connect(
+      "mongodb+srv://fescarvalho:f1f2j3p4@cluster0.5lsogdu.mongodb.net/?retryWrites=true&w=majority",
+    );
     console.log("connect to database 👌");
     return dbConn;
   } catch (e) {
